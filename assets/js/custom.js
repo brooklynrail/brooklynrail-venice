940b4149dca060cad97d88724efb6e4e4939a606
Jeremy Zilar <jeremyzilar@gmail.com>
2019-07-22 21:44:37 -0400

    add slick JS

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slick-track').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
+});
